df3f7c0d07e8cd8145fc91fe50f287562f3be4e8
Margo <marglodistovar@gmail.com>
2022-09-17 12:50:12 -0300

    Modificacion del Json carpeta raiz pages

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/contacto" element={<Contacto />} />
           <Route path="*" element={<NotFound />} />
         </Routes>        
